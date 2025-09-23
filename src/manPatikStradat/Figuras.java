145fdd7df7061de98fd91d5296a24b6163dcb012
KkManagalian <Dumbassyahoo2@gmail.com>
2025-09-23 11:20:53 +0300

    Pabeigta 1.klase :D

---
+++ src/manPatikStradat/Figuras.java
@@ -0,0 +1,21 @@
+package manPatikStradat;
+
+public class Figuras {
+
+	public int x, y;
+	
+	public void Centrs (int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+
+	public String izvaditKoordinates() {
+		return "Centra punkts atordas:\n x="+x+"\ny="+y;
+	}
+	
+	public void parvietot (int koordX, int koordY) {
+		x+=koordX;
+		y+=koordY;
+	}
+	
+}
